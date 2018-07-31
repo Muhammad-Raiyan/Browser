60e6f220da4998797ee309ef4de8d6d495ed8df8
Muhammad R Islam <muhammad.r.islam@stonybrook.edu>
2018-07-31 04:50:42 -0400

    UI sends KILL signal before closing to network abckend

--- BrowserNetwork/server.js
+++ BrowserNetwork/server.js
@@ -5,7 +5,9 @@ var synchronizeSubscription = zmq.socket('req');
 var synchronizePublisher = zmq.socket("rep");
 var page_req = require('request')
 
-var SUBSCRIBERS_EXPECTED = 1;
+const publisherEnvelope = "gui_backend"
+const subscriberEnvelope = "network_backend"
+const SUBSCRIBERS_EXPECTED = 1;
 
 // Subscription Synchronization
 synchronizeSubscription.connect('tcp://localhost:5100')
@@ -41,17 +43,10 @@ publisher.bind('tcp://*:6101', function (err) {
 
 // Function to handle requests
 function handle_request(request) {
-    var msg = [];
-    Array.prototype.slice.call(arguments).forEach(function (arg) {
-        msg.push(arg.toString());
-    });
-
-    var request = JSON.parse(msg[1])
     var url = request.url
-    var token = "gui_backend"
-
-    console.log("Token: " + token)
-    console.log("url: " + url)
+    var req_token =  request.token
+    console.log("Token: " + req_token)
+    console.log("url: " + url)  
 
     if (url.startsWith("www"))
         url = "https://" + url;
@@ -59,27 +54,39 @@ function handle_request(request) {
     page_req(url, function (error, response, body) {
         if (error) {
             console.log('error:', error);
-            publisher.send([token, error]);
+            publisher.send([publisherEnvelope, error]);
         } else if (response && response.statusCode == 200) {
             console.log('response status', response.statusCode);
-            publisher.send([token, body]);
+            publisher.send([publisherEnvelope, body]);
         } else if (response) {
             console.log('Reponse status not OK:', response.statusCode);
-            publisher.send([token, response.statusMessage]);
+            publisher.send([publisherEnvelope, response.statusMessage]);
         }
     });
-
 }
 
 // Initializing and listening to port
-subscriber.on("message", handle_request)
+subscriber.on("message", function(request){
+    var msg = [];
+    Array.prototype.slice.call(arguments).forEach(function (arg) {
+        msg.push(arg.toString());
+    });
+
+    var request = JSON.parse(msg[1])
+
+    if (request.token === "KILL"){
+        close_sockets();
+        process.exit();
+    } else {
+        handle_request(request);
+    }
+})
+
 subscriber.connect("tcp://localhost:5101");
-subscriber.subscribe("network_backend");
+subscriber.subscribe(subscriberEnvelope);
 
-
-
-// On terminate cleanup
-process.on('SIGINT', function () {
+// Function to close all sockets
+function close_sockets(){
     synchronizeSubscription.close()
     console.log('closed sync Subscription ')
     synchronizePublisher.close()
@@ -88,4 +95,9 @@ process.on('SIGINT', function () {
     console.log('Closed subscriber')
     publisher.close()
     console.log('Closed publisher')
+}
+
+// On terminate cleanup
+process.on('SIGINT', function () {
+    close_sockets();
 })
